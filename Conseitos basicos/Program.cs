060b160568c405d1834c4a30099570f4434b3dbb
Delta_John <eric.portela.costa@gmail.com>
2025-08-19 20:34:09 -0300

    informe o valor

--- Conseitos basicos/Program.cs
+++ Conseitos basicos/Program.cs
@@ -1,8 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-int idade = 24;
-Console.WriteLine("A idade é" + idade);
-if (idade >= 18)
-    Console.WriteLine("Você pode tirar habilitação");
+﻿try
+{
 
-else
-    Console.WriteLine("Você^não pode tirar habilitaçãõ");
+}
+
+  catch (Exception e)
+{
+
+}
+
+ Console.WriteLine("Informe seu nome");
+ string nome = Console.ReadLine();
+ Console.WriteLine("Informe seu salario");
+ decimal salario = decimal.Parse(Console.ReadLine());
+ Console.WriteLine("Seja bem vindo(a) {0}, o salario informado foi {1}", nome, salario);
+
+//INFORME O VALOR DO IRPF A SER PAGO DE ACORDO COM SALARIO INFORMADO. UTILIZE A TABELA DE ACORDO COM IRPF
+
+    decimal irpfDevido;
+ if (salario == 2259.20m)
+    irpfDevido = 0;
+ else if (salario <= 2826.65m)
+    irpfDevido = salario * 0.75m - 169.21m;
+ else if (salario <= 3751.05m)
+    irpfDevido = salario * 0.15m - 381.44m;
+ else if (salario <= 4664.68m)
+    irpfDevido = salario * 0.225m - 662.77m;
+ else
+    irpfDevido = salario * 0.275m - 896m;
+
+Console.WriteLine($"Para o salario de {salario} o valor a ser pago de IRPF e {irpfDevido}, então o salario com desconto de IRPF sera {salario - irpfDevido}");
+
+  
+  catch (FormatException)
+ {
+        Console.WriteLine("O salario deve ser informado em formato numerico");
+   }
+  catch (Exception e)
+ {
+    Console.WriteLine("Ocoreu um erro. Contate e suporte tecnico");
+    Console.WriteLine(e.StackTrace);
+ }
